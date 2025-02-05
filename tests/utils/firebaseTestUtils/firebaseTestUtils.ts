bd50a0946556064ba7e822821c69a05a30ee1ac7
robmolloy <robmolloy@hotmail.co.uk>
2025-02-05 16:50:53 +0000

    remove unused firestoreTestUtil

--- tests/utils/firebaseTestUtils/firebaseTestUtils.ts
+++ tests/utils/firebaseTestUtils/firebaseTestUtils.ts
@@ -23,27 +23,6 @@ export const createTestEnvironment = async (p: { projectId: string }) => {
   });
 };
 
-export async function expectPermissionDenied(
-  promise: Promise<unknown>,
-  options?: { onError?: () => void }
-) {
-  return new Promise<void>(async (resolve) => {
-    const hasFailed = await (async () => {
-      try {
-        const errorResult = await assertFails(promise);
-        return ["permission-denied", "PERMISSION_DENIED"].includes(errorResult.code);
-      } catch (error) {
-        return false;
-      }
-    })();
-    if (!hasFailed) {
-      if (options?.onError) options.onError();
-      else throw new Error("Expected request to fail, but it succeeded.");
-    } else expect(hasFailed).toBe(true);
-    resolve(undefined);
-  });
-}
-
 export async function isRequestDenied(promise: Promise<unknown>) {
   try {
     const errorResult = await assertFails(promise);
