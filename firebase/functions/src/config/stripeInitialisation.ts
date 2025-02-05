2aa47bce25ef60e4ac3204c30744e2ca2e1a5504
robmolloy <robmolloy@hotmail.co.uk>
2025-02-05 22:34:56 +0000

    stripe initialisations

---
+++ firebase/functions/src/config/stripeInitialisation.ts
@@ -0,0 +1,5 @@
+import { Stripe } from "stripe";
+
+const stripeSecretKey =
+  "sk_test_51QhH4nIGFJRyk0RhUnRTVsXZICgwBLG5C6tiDecTJNR5MC40Skm1y3HMQt0HQA0dEdReAcEH3v2TozuJ9mlLHBQM00d3N3noeZ";
+export const stripe = new Stripe(stripeSecretKey);
--- firebase/functions/src/routes/createStripePaymentIntentRoute.ts
+++ firebase/functions/src/routes/createStripePaymentIntentRoute.ts
@@ -1,13 +1,9 @@
 import { onCall } from "firebase-functions/v2/https";
 import { fail } from "../utils/devUtils";
 
-import { Stripe } from "stripe";
 import z from "zod";
+import { stripe } from "../config/stripeInitialisation";
 import { stripeSdk } from "../sdks/stripeSdk/stripeSdk";
-
-const stripeSecretKey =
-  "sk_test_51QhH4nIGFJRyk0RhUnRTVsXZICgwBLG5C6tiDecTJNR5MC40Skm1y3HMQt0HQA0dEdReAcEH3v2TozuJ9mlLHBQM00d3N3noeZ";
-const stripe = new Stripe(stripeSecretKey);
 
 const requestDataSchema = z.object({ amount: z.number() });
 
